397d1ac7c59b2b4590abd5d6f79c1ca2f8dea438
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-12 14:11:04 +0200

    split off creation of steering

--- magnetic_deflection/discovery.py
+++ magnetic_deflection/discovery.py
@@ -77,6 +77,48 @@ def _great_circle_distance_long_lat(lam_long1, phi_alt1, lam_long2, phi_alt2):
     return delta_sigma
 
 
+def make_corsika_steering(
+    run_id,
+    site,
+    primary_particle_id,
+    primary_energy,
+    primary_cone_azimuth_deg,
+    primary_cone_zenith_deg,
+    primary_cone_opening_angle_deg,
+    num_events,
+    prng,
+):
+    steering = {}
+    steering["run"] = {
+        "run_id": int(run_id),
+        "event_id_of_first_event": 1,
+        "observation_level_asl_m": site["observation_level_asl_m"],
+        "earth_magnetic_field_x_muT": site["earth_magnetic_field_x_muT"],
+        "earth_magnetic_field_z_muT": site["earth_magnetic_field_z_muT"],
+        "atmosphere_id": site["atmosphere_id"],
+    }
+    steering["primaries"] = []
+    for event_id in range(num_events):
+        az, zd = cpw.random_distributions.draw_azimuth_zenith_in_viewcone(
+            prng=prng,
+            azimuth_rad=np.deg2rad(primary_cone_azimuth_deg),
+            zenith_rad=np.deg2rad(primary_cone_zenith_deg),
+            min_scatter_opening_angle_rad=np.deg2rad(0.0),
+            max_scatter_opening_angle_rad=np.deg2rad(primary_cone_opening_angle_deg),
+            max_zenith_rad=np.deg2rad(90),
+        )
+        prm = {
+            "particle_id": int(primary_particle_id),
+            "energy_GeV": float(primary_energy),
+            "zenith_rad": zd,
+            "azimuth_rad": az,
+            "depth_g_per_cm2": 0.0,
+            "random_seed": cpw.simple_seed(event_id + run_id * num_events),
+        }
+        steering["primaries"].append(prm)
+    return steering
+
+
 def estimate_cherenkov_pool(
     corsika_primary_steering, corsika_primary_path, min_num_cherenkov_photons,
 ):
@@ -94,8 +136,8 @@ def estimate_cherenkov_pool(
         corsika_run = cpw.CorsikaPrimary(
             corsika_path=corsika_primary_path,
             steering_dict=corsika_primary_steering,
-            stdout_path=os.path.join(tmp_dir + ".stdout"),
-            stderr_path=os.path.join(tmp_dir + ".stderr"),
+            stdout_path=os.path.join(tmp_dir, "corsika.stdout"),
+            stderr_path=os.path.join(tmp_dir, "corsika.stderr"),
         )
 
         for idx, airshower in enumerate(corsika_run):
@@ -154,34 +196,17 @@ def direct_discovery(
         azimuth_deg=instrument_azimuth_deg, zenith_deg=instrument_zenith_deg
     )
 
-    steering = {}
-    steering["run"] = {
-        "run_id": int(run_id),
-        "event_id_of_first_event": 1,
-        "observation_level_asl_m": site["observation_level_asl_m"],
-        "earth_magnetic_field_x_muT": site["earth_magnetic_field_x_muT"],
-        "earth_magnetic_field_z_muT": site["earth_magnetic_field_z_muT"],
-        "atmosphere_id": site["atmosphere_id"],
-    }
-    steering["primaries"] = []
-    for event_id in range(num_events):
-        az, zd = cpw.random_distributions.draw_azimuth_zenith_in_viewcone(
-            prng=prng,
-            azimuth_rad=np.deg2rad(best_primary_azimuth_deg),
-            zenith_rad=np.deg2rad(best_primary_zenith_deg),
-            min_scatter_opening_angle_rad=np.deg2rad(0.0),
-            max_scatter_opening_angle_rad=np.deg2rad(spray_radius_deg),
-            max_zenith_rad=np.deg2rad(90),
-        )
-        prm = {
-            "particle_id": int(primary_particle_id),
-            "energy_GeV": float(primary_energy),
-            "zenith_rad": zd,
-            "azimuth_rad": az,
-            "depth_g_per_cm2": 0.0,
-            "random_seed": cpw.simple_seed(event_id + run_id * num_events),
-        }
-        steering["primaries"].append(prm)
+    steering = make_corsika_steering(
+        run_id=run_id,
+        site=site,
+        primary_particle_id=primary_particle_id,
+        primary_energy=primary_energy,
+        primary_cone_azimuth_deg=best_primary_azimuth_deg,
+        primary_cone_zenith_deg=best_primary_zenith_deg,
+        primary_cone_opening_angle_deg=spray_radius_deg,
+        num_events=num_events,
+        prng=prng,
+    )
 
     cherenkov_pools = estimate_cherenkov_pool(
         corsika_primary_steering=steering,
