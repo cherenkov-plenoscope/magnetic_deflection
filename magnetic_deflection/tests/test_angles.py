ed800b4232bab457ee097a7a6f8508db4ecf2cd8
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-13 13:52:15 +0200

    adopt in tests

--- magnetic_deflection/tests/test_angles.py
+++ magnetic_deflection/tests/test_angles.py
@@ -1,18 +1,19 @@
 import magnetic_deflection as mdfl
+from magnetic_deflection import spherical_coordinates as sphcors
 import numpy as np
 
 
 def test_azimuth_range():
-    assert 0 == mdfl.discovery._azimuth_range(0)
-    assert 90 == mdfl.discovery._azimuth_range(90)
-    assert 180 == mdfl.discovery._azimuth_range(180)
-    assert -179 == mdfl.discovery._azimuth_range(181)
-    assert -90 == mdfl.discovery._azimuth_range(-90)
-    assert 180 == mdfl.discovery._azimuth_range(180)
+    assert 0 == sphcors._azimuth_range(0)
+    assert 90 == sphcors._azimuth_range(90)
+    assert 180 == sphcors._azimuth_range(180)
+    assert -179 == sphcors._azimuth_range(181)
+    assert -90 == sphcors._azimuth_range(-90)
+    assert 180 == sphcors._azimuth_range(180)
 
 
 def test_azimuth_range_array():
-    arr = mdfl.discovery._azimuth_range(np.array([0.0, 90.0, 180.0, 181.0]))
+    arr = sphcors._azimuth_range(np.array([0.0, 90.0, 180.0, 181.0]))
     assert arr[0] == 0.0
     assert arr[1] == 90.0
     assert arr[2] == 180.0
@@ -23,15 +24,15 @@ def test_angles_scalars():
     for az_deg in np.linspace(-380, 380, 25):
         for zd_deg in np.linspace(0, 89, 25):
 
-            cx, cy = mdfl.discovery._az_zd_to_cx_cy(
+            cx, cy = sphcors._az_zd_to_cx_cy(
                 azimuth_deg=az_deg, zenith_deg=zd_deg
             )
 
-            az_deg_back, zd_deg_back = mdfl.discovery._cx_cy_to_az_zd_deg(
+            az_deg_back, zd_deg_back = sphcors._cx_cy_to_az_zd_deg(
                 cx=cx, cy=cy
             )
 
-            dist_deg = mdfl.discovery._angle_between_az_zd_deg(
+            dist_deg = sphcors._angle_between_az_zd_deg(
                 az1_deg=az_deg,
                 zd1_deg=zd_deg,
                 az2_deg=az_deg_back,
@@ -47,13 +48,13 @@ def test_angles_arrays():
     az_deg = np.linspace(-380, 380, num)
     zd_deg = np.linspace(0, 89, num)
 
-    cx, cy = mdfl.discovery._az_zd_to_cx_cy(
+    cx, cy = sphcors._az_zd_to_cx_cy(
         azimuth_deg=az_deg, zenith_deg=zd_deg
     )
 
-    az_deg_back, zd_deg_back = mdfl.discovery._cx_cy_to_az_zd_deg(cx=cx, cy=cy)
+    az_deg_back, zd_deg_back = sphcors._cx_cy_to_az_zd_deg(cx=cx, cy=cy)
 
-    dist_deg = mdfl.discovery._angle_between_az_zd_deg(
+    dist_deg = sphcors._angle_between_az_zd_deg(
         az1_deg=az_deg,
         zd1_deg=zd_deg,
         az2_deg=az_deg_back,
