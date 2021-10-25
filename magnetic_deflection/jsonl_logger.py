552292c17fafa53fe680ede0152524dd9e88dda4
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-25 10:28:50 +0200

    just 'w', no 'a'

--- magnetic_deflection/jsonl_logger.py
+++ magnetic_deflection/jsonl_logger.py
@@ -1,11 +1,8 @@
 import logging
-import os
 
 def init(path):
-    if os.path.exists(path):
-        os.remove(path)
     l = logging.Logger(name=path)
-    file_handler = logging.FileHandler(filename=path, mode="a")
+    file_handler = logging.FileHandler(filename=path, mode="w")
     datefmt_iso8601 = "%Y-%m-%dT%H:%M:%S"
     formatter = logging.Formatter(
         fmt='{"t":"%(asctime)s", "m":"%(message)s"}', datefmt=datefmt_iso8601,
