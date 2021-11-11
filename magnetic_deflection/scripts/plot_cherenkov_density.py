56777200bb48c54da4091893564deea03c3b85a8
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-11-11 12:16:10 +0100

    concentrate plotting config

--- magnetic_deflection/scripts/plot_cherenkov_density.py
+++ magnetic_deflection/scripts/plot_cherenkov_density.py
@@ -23,6 +23,8 @@ os.makedirs(out_dir, exist_ok=True)
 shower_statistics = mdfl.read_shower_statistics(work_dir=work_dir)
 c = mdfl.read_config(work_dir=work_dir)
 
+MIN_NUM_SHOWER = 11
+
 density_map = {
     "num_photons": {"label": "intensity$\,/\,$1", "lim": [1e2, 1e6],},
     "spread_area_m2": {"label": "area$\,/\,$m$^{2}$", "lim": [1e4, 1e7],},
@@ -36,36 +38,64 @@ density_map = {
     },
 }
 
-parmap = {"gamma": "k", "electron": "b", "proton": "r", "helium": "orange"}
+pc = {
+    "sites": {
+        "namibia": {
+            "nice_label": "Gamsberg",
+            "marker": "+",
+            "linestyle": "--",
+        },
+        "chile": {
+            "nice_label": "Chajnantor",
+            "marker": "*",
+            "linestyle": ":",
+        },
+        "namibiaOff": {
+            "nice_label": "Gamsberg-Off",
+            "marker": ".",
+            "linestyle": "-.",
+        },
+        "lapalma": {
+            "nice_label": "Roque",
+            "marker": "^",
+            "linestyle": "-",
+        },
+    },
+    "particles": {
+        "gamma": {
+            "color": "black",
+            "nice_label": "gamma-ray",
+        },
+        "electron": {
+            "color": "blue",
+            "nice_label": "electron",
+        },
+        "proton": {
+            "color": "red",
+            "nice_label": "proton",
+        },
+        "helium": {
+            "color": "orange",
+            "nice_label": "helium",
+        },
+    }
+}
 
-MIN_NUM_SHOWER = 11
+ENERGY = {}
+ENERGY["fine"] = {}
+ENERGY["fine"]["num_bins"] = 60
+ENERGY["fine"]["bin_edges"] = np.geomspace(
+    1e-1, 1e2, ENERGY["fine"]["num_bins"] + 1
+)
+ENERGY["coarse"] = {}
+ENERGY["coarse"]["num_bins"] = 10
+ENERGY["coarse"]["bin_edges"] = np.geomspace(
+    1e-1, 1e2, ENERGY["coarse"]["num_bins"] + 1
+)
 
-# compute density
-# ---------------
-cherenkov_density = {}
 for skey in shower_statistics:
-    cherenkov_density[skey] = {}
     for pkey in shower_statistics[skey]:
-        sst = shower_statistics[skey][pkey]
-
-        den = {}
-        den["num_photons"] = sst["num_photons"]
-        den["spread_area_m2"] = (
-            np.pi * sst["position_std_major_m"] * sst["position_std_minor_m"]
-        )
-        den["spread_solid_angle_deg2"] = (
-            np.pi
-            * np.rad2deg(sst["direction_std_major_rad"])
-            * np.rad2deg(sst["direction_std_minor_rad"])
-        )
-        den["light_field_outer_density"] = sst["num_photons"] / (
-            den["spread_solid_angle_deg2"] * den["spread_area_m2"]
-        )
-
-        cherenkov_density[skey][pkey] = pd.DataFrame(den).to_records(
-            index=False
-        )
-
+        statkeys = list(shower_statistics[skey][pkey].dtype.names)
 
 def make_off_axis_angle_deg(
     cherenkov_direction_med_cx_rad,
@@ -87,21 +117,96 @@ def make_off_axis_angle_deg(
         zd2_deg=pointing_zenith_deg,
     )
 
+# cut on-axis
+# -----------
+on_axis_shower_statistics = {}
+for skey in shower_statistics:
+    on_axis_shower_statistics[skey] = {}
+    for pkey in shower_statistics[skey]:
 
-ENERGY = {}
-ENERGY["fine"] = {}
-ENERGY["fine"]["num_bins"] = 60
-ENERGY["fine"]["bin_edges"] = np.geomspace(
-    1e-1, 1e2, ENERGY["fine"]["num_bins"] + 1
-)
-ENERGY["coarse"] = {}
-ENERGY["coarse"]["num_bins"] = 10
-ENERGY["coarse"]["bin_edges"] = np.geomspace(
-    1e-1, 1e2, ENERGY["coarse"]["num_bins"] + 1
-)
+        sst = shower_statistics[skey][pkey]
 
-num_energy_bins = 60
-energy_bin_edges = np.geomspace(1e-1, 1e2, num_energy_bins + 1)
+        off_axis_deg = make_off_axis_angle_deg(
+            cherenkov_direction_med_cx_rad=sst["direction_med_cx_rad"],
+            cherenkov_direction_med_cy_rad=sst["direction_med_cy_rad"],
+            pointing_azimuth_deg=c["pointing"]["azimuth_deg"],
+            pointing_zenith_deg=c["pointing"]["zenith_deg"],
+        )
+
+        on_axis_mask = (
+            off_axis_deg
+            <= 2
+            * c["particles"][pkey]["magnetic_deflection_max_off_axis_deg"]
+        )
+        on_axis_shower_statistics[skey][pkey] = shower_statistics[skey][pkey][on_axis_mask]
+
+del(shower_statistics)
+
+
+for tkey in statkeys:
+    for skey in on_axis_shower_statistics:
+
+        fig = sebplt.figure(figsize)
+        ax = sebplt.add_axes(fig=fig, span=(0.15, 0.2, 0.8, 0.75))
+
+        for pkey in on_axis_shower_statistics[skey]:
+            oasst = on_axis_shower_statistics[skey][pkey]
+            ax.plot(
+                oasst["particle_energy_GeV"],
+                oasst[tkey],
+                marker=".",
+                markersize=0.1,
+                linewidth=0.0,
+                color=pc["particles"][pkey]["color"],
+                alpha=0.1,
+            )
+
+        ax.semilogx()
+        ax.spines["right"].set_visible(False)
+        ax.spines["top"].set_visible(False)
+        ax.set_xlabel("energy$\,/\,$GeV")
+        ax.set_ylabel(tkey)
+        ax.set_xlim(
+            [
+                min(ENERGY["fine"]["bin_edges"]),
+                max(ENERGY["fine"]["bin_edges"]),
+            ]
+        )
+        #ax.set_ylim(1e2, 1e6)
+        ax.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+        fig.savefig(os.path.join(out_dir, "{:s}_{:s}.jpg".format(skey, tkey)))
+        plt.close(fig)
+
+
+
+
+# compute density
+# ---------------
+cherenkov_density = {}
+for skey in on_axis_shower_statistics:
+    cherenkov_density[skey] = {}
+    for pkey in on_axis_shower_statistics[skey]:
+        oasst = on_axis_shower_statistics[skey][pkey]
+
+        den = {}
+        den["num_photons"] = oasst["num_photons"]
+        den["spread_area_m2"] = (
+            np.pi * oasst["position_std_major_m"] * oasst["position_std_minor_m"]
+        )
+        den["spread_solid_angle_deg2"] = (
+            np.pi
+            * np.rad2deg(oasst["direction_std_major_rad"])
+            * np.rad2deg(oasst["direction_std_minor_rad"])
+        )
+        den["light_field_outer_density"] = oasst["num_photons"] / (
+            den["spread_solid_angle_deg2"] * den["spread_area_m2"]
+        )
+
+        cherenkov_density[skey][pkey] = pd.DataFrame(den).to_records(
+            index=False
+        )
+
+
 
 # cut on-axis and bin in energy
 # -----------------------------
@@ -116,20 +221,7 @@ for fkey in ENERGY:
         ooo[fkey][skey] = {}
         for pkey in cherenkov_density[skey]:
             ooo[fkey][skey][pkey] = {}
-            sst = shower_statistics[skey][pkey]
-
-            off_axis_deg = make_off_axis_angle_deg(
-                cherenkov_direction_med_cx_rad=sst["direction_med_cx_rad"],
-                cherenkov_direction_med_cy_rad=sst["direction_med_cy_rad"],
-                pointing_azimuth_deg=c["pointing"]["azimuth_deg"],
-                pointing_zenith_deg=c["pointing"]["zenith_deg"],
-            )
-
-            on_axis_mask = (
-                off_axis_deg
-                <= 2
-                * c["particles"][pkey]["magnetic_deflection_max_off_axis_deg"]
-            )
+            oasst = on_axis_shower_statistics[skey][pkey]
 
             for dkey in density_map:
                 ooo[fkey][skey][pkey][dkey] = {
@@ -143,17 +235,16 @@ for fkey in ENERGY:
                 E_start = ENERGY[fkey]["bin_edges"][ebin]
                 E_stop = ENERGY[fkey]["bin_edges"][ebin + 1]
                 E_mask = np.logical_and(
-                    sst["particle_energy_GeV"] >= E_start,
-                    sst["particle_energy_GeV"] < E_stop,
+                    oasst["particle_energy_GeV"] >= E_start,
+                    oasst["particle_energy_GeV"] < E_stop,
                 )
-                mask = np.logical_and(E_mask, on_axis_mask)
-                num_samples = np.sum(mask)
+                num_samples = np.sum(E_mask)
 
                 print(skey, pkey, "E-bin:", ebin, "num. shower:", num_samples)
 
                 for dkey in density_map:
                     cde = cherenkov_density[skey][pkey][dkey]
-                    valid_cde = cde[mask]
+                    valid_cde = cde[E_mask]
 
                     if num_samples >= MIN_NUM_SHOWER:
                         p84 = np.percentile(valid_cde, 50 + 68 / 2)
@@ -170,12 +261,12 @@ ooc = ooo["coarse"]
 alpha = 0.2
 
 for dkey in density_map:
-    for skey in shower_statistics:
+    for skey in cherenkov_density:
 
         fig = sebplt.figure(figsize)
         ax = sebplt.add_axes(fig=fig, span=(0.15, 0.2, 0.8, 0.75))
 
-        for pkey in parmap:
+        for pkey in cherenkov_density[skey]:
 
             for ebin in range(num_energy_bins):
                 E_start = ENERGY["fine"]["bin_edges"][ebin]
@@ -189,14 +280,14 @@ for dkey in density_map:
                     ax.fill(
                         [E_start, E_start, E_stop, E_stop],
                         [p16, p84, p84, p16],
-                        color=parmap[pkey],
+                        color=pc["particles"][pkey]["color"],
                         alpha=alpha,
                         linewidth=0.0,
                     )
                     ax.plot(
                         [E_start, E_stop],
                         [p50, p50],
-                        color=parmap[pkey],
+                        color=pc["particles"][pkey]["color"],
                         alpha=1.0,
                     )
         ax.loglog()
@@ -218,12 +309,12 @@ for dkey in density_map:
 
 # statistics
 # ----------
-for skey in shower_statistics:
+for skey in oof:
 
     fig = sebplt.figure(figsize)
     ax = sebplt.add_axes(fig=fig, span=(0.15, 0.2, 0.8, 0.75))
 
-    for pkey in parmap:
+    for pkey in oof[skey]:
         for ebin in range(num_energy_bins):
             E_start = ENERGY["fine"]["bin_edges"][ebin]
             E_stop = ENERGY["fine"]["bin_edges"][ebin + 1]
@@ -232,13 +323,13 @@ for skey in shower_statistics:
             ax.plot(
                 [E_start, E_stop],
                 [count, count],
-                color=parmap[pkey],
+                color=pc["particles"][pkey]["color"],
                 alpha=1.0,
             )
             ax.fill(
                 [E_start, E_start, E_stop, E_stop],
                 [0, count, count, 0],
-                color=parmap[pkey],
+                color=pc["particles"][pkey]["color"],
                 alpha=alpha,
                 linewidth=0.0,
             )
@@ -260,47 +351,36 @@ for skey in shower_statistics:
 # density by side
 # ---------------
 
-nice_site_labels = {
-    # "namibiaOff": "Gamsberg-Off",
-    "namibia": "Gamsberg",
-    "chile": "Chajnantor",
-    # "lapalma": "Roque",
-}
-
 dkey = "light_field_outer_density"
-
-sitemap = {"namibia": "+", "lapalma": "^", "chile": "*", "namibiaOff": "."}
-
-particle_colors = {
-    "electron": "blue",
-    "gamma": "black",
-}
 
 fig = sebplt.figure(figsize)
 ax = sebplt.add_axes(fig=fig, span=(0.15, 0.2, 0.8, 0.75))
 
-for skey in nice_site_labels:
-    for pkey in particle_colors:
+for skey in ooc:
+    for pkey in ooc[skey]:
 
-        if particle_colors[pkey] == "black":
-            label = nice_site_labels[skey]
+        if pc["particles"][pkey]["color"] == "black":
+            site_label = pc["sites"][skey]["nice_label"]
         else:
-            label = None
+            site_label = None
 
         ax.plot(
             ENERGY["coarse"]["bin_edges"][0:-1],
             ooc[skey][pkey][dkey]["percentile50"],
-            sitemap[skey],
-            color=particle_colors[pkey],
+            marker=pc["sites"][skey]["marker"],
+            linewidth=0.0,
+            color=pc["particles"][pkey]["color"],
             alpha=0.5,
-            label=label,
+            label=site_label,
         )
 
         ax.plot(
             ENERGY["coarse"]["bin_edges"][0:-1],
             ooc[skey][pkey][dkey]["percentile50"],
-            "-",
-            color=particle_colors[pkey],
+            marker=None,
+            linestyle=pc["sites"][skey]["linestyle"],
+            linewidth=0.5,
+            color=pc["particles"][pkey]["color"],
             alpha=0.33,
         )
 
