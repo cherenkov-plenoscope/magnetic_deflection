b317c20c092bf41627c2836fcfabfae644218878
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-10-31 16:27:50 +0100

    is a part of analysis

--- magnetic_deflection/cherenkov_pool_analysis.py
+++ magnetic_deflection/cherenkov_pool_analysis.py
@@ -92,3 +92,29 @@ def make_cos_theta_wrt_center_direction(
     del dot_product
     assert len(cos_theta) == len(photon_cx_rad)
     return cos_theta
+
+
+def init_ellipse(x, y):
+    median_x = np.median(x)
+    median_y = np.median(y)
+
+    cov_matrix = np.cov(np.c_[x, y].T)
+    eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
+    major_idx = np.argmax(eigen_values)
+    if major_idx == 0:
+        minor_idx = 1
+    else:
+        minor_idx = 0
+
+    major_axis = eigen_vectors[:, major_idx]
+    major_std = np.sqrt(np.abs(eigen_values[major_idx]))
+    minor_std = np.sqrt(np.abs(eigen_values[minor_idx]))
+
+    azimuth = np.arctan2(major_axis[0], major_axis[1])
+    return {
+        "median_cx": median_cx,
+        "median_cy": median_cy,
+        "azimuth": azimuth,
+        "major_std": major_std,
+        "minor_std": minor_std,
+    }
