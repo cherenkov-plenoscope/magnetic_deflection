6d0da015bc3730cff956edcdd8bcd55f31a1d4c9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-11-13 16:34:11 +0100

    export csv tables

--- magnetic_deflection/production.py
+++ magnetic_deflection/production.py
@@ -1,6 +1,8 @@
 import atmospheric_cherenkov_response as acr
 from . import allsky
 import os
+import json_numpy
+import rename_after_writing as rnw
 
 
 def init(work_dir, corsika_primary_path):
@@ -45,3 +47,23 @@ def run(work_dir, pool, num_runs=960, num_showers_per_run=1280):
                 num_jobs=num_runs,
                 num_showers_per_job=num_showers_per_run,
             )
+
+
+def export_csv(work_dir, out_dir):
+    os.makedirs(out_dir, exist_ok=True)
+
+    for sk in acr.sites.keys():
+        site_path = os.path.join(out_dir, "{:s}.json".format(sk))
+        sk_dir = os.path.join(work_dir, sk)
+        for pk in acr.particles.keys():
+            sk_pk_dir = os.path.join(sk_dir, pk)
+
+            print(sk, pk)
+            sky = allsky.open(sk_pk_dir)
+
+            with rnw.open(site_path, "wt") as f:
+                f.write(json_numpy.dumps(sky.config["site"], indent=4))
+
+            out_path = os.path.join(out_dir, "{:s}_{:s}.csv".format(sk, pk))
+            if not os.path.exists(out_path):
+                sky.store.export_csv(path=out_path)
